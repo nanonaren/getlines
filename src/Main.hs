d38eeb3179b9f1b1d2104c6a932be8816cbf372f
Naren Sundaravaradan <nano.naren@gmail.com>
2010-11-20 14:16:55 -0500

    New argument parsing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,43 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main
     (
+      main
     ) where
 
 import System.IO
 import Data.List (sort)
-import System.Console.ParseArgs
+import System.Console.CmdArgs
 import System.Directory (doesFileExist)
 
-arguments :: [Arg String]
-arguments =
-    [
-     Arg "nosort" (Just 'n') (Just "nosort") Nothing
-             "Does not sort the provided line numbers"
-    ]
+data Options = Options
+    {
+      noSort :: Bool,
+      file :: FilePath
+    } deriving (Show,Data,Typeable)
+
+opts = Options
+       {
+         noSort = def &= help "Do not sort the given line numbers.",
+         file = def &= argPos 0 &= typFile
+       } &= summary "getlines v0.1, (C) Naren Sundar 2010"
+         &= program "getlines"
+         &= details ["EXAMPLE: get lines 37 and 43 from file.",
+                     "$ echo \"37 43\" | getlines file","","",
+                     "http://github.com/nanonaren"]
 
 main = do
-  ags <- parseArgsIO ArgsTrailing arguments
-  let nosort = gotArg ags "nosort"
-      inpfile = let xs = argsRest ags in if null xs then "" else head xs
-
+  options <- cmdArgs opts
   --check file exists
-  exists <- doesFileExist inpfile
+  exists <- doesFileExist (file options)
   if not exists then fail "Cannot open input file" else return ()
 
   -- read lines
-  lns <- fmap lines $ readFile inpfile
+  lns <- fmap lines $ readFile (file options)
   lineNos <- fmap (map read.words) $ hGetContents stdin
 
   -- filter lines
   mapM_ putStrLn $
-        filterLines (if nosort then lineNos else sort lineNos) lns
+        filterLines (if (noSort options) then lineNos else sort lineNos) lns
 
 filterLines ns ls = filterLines' ns (zip [1..] ls)
 filterLines' [] _ = []
